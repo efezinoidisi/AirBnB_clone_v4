75eed45ee4fa6d9c646696dd2e8e42602fe0f10f
MikeRock51 <mikerockmusic51@gmail.com>
2023-05-07 07:39:54 +0100

    Completed TAsk 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,66 @@
+$(document).ready(() => {
+  const checkedAmenities = {};
+  const checkboxes = $('input#amenity_checkbox');
+  const filterList = $('div.amenities h4');
+  const statusUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  const statusBar = $('div#api_status');
+  const placeSearchUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+
+  $.ajax({
+    url: placeSearchUrl,
+    type: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: '{}',
+    success: (places) => {
+      $.each(places, (index, place) => {
+        $('section.places').append(`<article>
+          <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+          </div>
+          <div class="information">
+            <div class="max_guest">${place.max_guest} Guest(s)</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>
+          </div>
+          <div class="user">
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`);
+      });
+    },
+    error: (err) => {
+      console.log("Failed Request:", err);
+    }
+  });
+
+  $.get(statusUrl, (response) => {
+    if (response.status === "OK") {
+      statusBar.addClass('available');
+    } else {
+      statusBar.removeClass('available');
+    }
+  });
+
+  $.each(checkboxes, (index, checkbox) => {
+    $(checkbox).on('change', () => {
+      if ($(checkbox).is(':checked')) {
+        checkedAmenities[$(checkbox).data('name')] = $(checkbox).data('id');
+      } else {
+        delete checkedAmenities[$(checkbox).data('name')];
+      }
+      filterList.text("");
+      let numItems = 0;
+      $.each(checkedAmenities, (key, value) => {
+        if (numItems > 0) {
+          filterList.append(', ');
+        }
+        filterList.append(key);
+        numItems++;
+      });
+    });
+  });
+});
